8d815e44ac4b76851588220f5e68a5a2a55a1a5e
sandeepsudula9 <sandeepsudula75@gmail.com>
2026-03-04 19:04:15 +0530

    Phase 1 cleanup: remove 33 dead files + exclude audio/video binaries from git

--- local_ai_assistant/test_email_query.py
+++
@@ -1,7 +0,0 @@
-from agents.knowledge.email_query_agent import search_emails, list_summaries
-
-print("Search 'Google':")
-print(search_emails("Google"))
-
-print("\nAll summaries:")
-print(list_summaries())
--- local_ai_assistant/test_email_summary.py
+++
@@ -1,3 +0,0 @@
-from agents.knowledge.email_summarizer_agent import handle_email_summary
-
-print(handle_email_summary())
